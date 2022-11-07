3574b9c52ba7b667d46936c19ecd14bc81d38196
Ícaro Augusto Maccari Zelioli <icaro.zelioli@radixeng.com.br>
2022-11-07 10:59:24 -0300

    Completes the deploy step

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 estilos = [
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://fonts.googleapis.com/icon?family=Material+Icons",
-    dbc.themes.COSMO
+    dbc.themes.DARKLY
 ]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 # FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
--- components/dashboards.py
+++ components/dashboards.py
@@ -5,7 +5,10 @@ from dash import html, dcc
 import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
+
+# set graph margin
+graph_margin=dict(l=25, r=25, t=25, b=0)
 
 # set dashboard layout
 layout = dbc.Col([
@@ -23,7 +26,7 @@ layout = dbc.Col([
                 html.H6('-', id='p-prob', style={})
             ], style={'padding-left': '20px', 'padding-top': '10px'})
         ], width=6)
-    ], style={'margin': '10px'}),
+    ], style={'margin': '0.5px'}),
     # plot the evolution of RUL prediction
     dbc.Col(
         dbc.Card(dcc.Graph(id='rul_history'), style={'height': '100%', 'padding': '10px', 'margin': '10px'}),
@@ -35,3 +38,151 @@ layout = dbc.Col([
         width=12
     ),
 ])
+
+# function callbacks -------------------------------------------------------------------------------------------------
+# update RUL prediction
+@app.callback(
+    Output('p-rul', 'children'),
+    [
+        Input('send_data', 'n_clicks'),
+        Input('store-predictions', 'data')
+    ]
+)
+def predicted_rul(n, data) -> str:
+
+    # transform data back to dataframe
+    pred = pd.DataFrame(data)
+
+    # after the click, take the last predicted RUL
+    if n:
+        pred_rul = pred.tail(1)['rul_pred'].values[0]
+    else:
+        pred_rul = 0
+
+    # create text to return
+    text = f'RUL = {int(pred_rul)} cycles (+- 40 cycles)'
+
+    return text
+
+# update probability of failure
+@app.callback(
+    Output('p-prob', 'children'),
+    [
+        Input('send_data', 'n_clicks'),
+        Input('store-predictions', 'data')
+    ]
+)
+def predicted_rul(n, data) -> str:
+
+    # transform data back to dataframe
+    pred = pd.DataFrame(data)
+
+    # after the click, take the last predicted RUL
+    if n:
+        pred_prob = pred.tail(1)['prob_pred'].values[0]
+    else:
+        pred_prob = 0
+
+    # create text to return
+    text = f'P = {round(pred_prob * 100, 2)} % (in the next 50 cycles)'
+
+    return text
+
+# update RUL history plot
+@app.callback(
+    Output('rul_history', 'figure'),
+    [
+        Input('store-predictions', 'data')
+    ]
+)
+def update_rul_history_plot(data):
+
+    # transform data back to dataframe
+    pred = pd.DataFrame(data)
+
+    # build plot
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Line(name='Predicted RUL', 
+        x = list(range(pred.shape[0])),
+        y = pred['rul_pred']))
+
+    fig.add_shape(
+        type='line',
+        x0=0,
+        y0=50,
+        x1=pred.shape[0]-1,
+        y1=50,
+        line=dict(
+            color='red',
+            dash='dash',
+            width=2
+        )
+    )
+    fig.update_layout(margin=graph_margin)
+    fig.update_yaxes(title_text='RUL Prediction')
+    fig.update_xaxes(title_text='# Executions')
+
+    return fig
+
+# update probability history plot
+@app.callback(
+    Output('prob_history', 'figure'),
+    [
+        Input('store-predictions', 'data')
+    ]
+)
+def update_rul_history_plot(data):
+
+    # transform data back to dataframe
+    pred = pd.DataFrame(data)
+
+    # build plot
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Line(name='Probability of Failure', 
+        x = list(range(pred.shape[0])),
+        y = pred['prob_pred']))
+    fig.add_shape(
+        type='line',
+        x0=0,
+        y0=0.01,
+        x1=pred.shape[0]-1,
+        y1=0.01,
+        line=dict(
+            color='yellow',
+            dash='dash',
+            width=2
+        )
+    )
+    fig.add_shape(
+        type='line',
+        x0=0,
+        y0=0.05,
+        x1=pred.shape[0]-1,
+        y1=0.05,
+        line=dict(
+            color='orange',
+            dash='dash',
+            width=2
+        )
+    )
+    fig.add_shape(
+        type='line',
+        x0=0,
+        y0=0.1,
+        x1=pred.shape[0]-1,
+        y1=0.1,
+        line=dict(
+            color='red',
+            dash='dash',
+            width=2
+        )
+    )
+    fig.update_layout(margin=graph_margin)
+    fig.update_yaxes(title_text='Prediction of Probability of Failure')
+    fig.update_xaxes(title_text='# Executions')
+
+    return fig
